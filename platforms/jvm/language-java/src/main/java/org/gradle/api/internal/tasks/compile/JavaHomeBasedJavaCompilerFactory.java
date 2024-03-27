630f8b6f947f2f2e681025872e07b8c6d083b0b3
Bálint Hegyi <bhegyi@gradle.com>
2024-03-27 10:18:23 +0000

    Revert JavaHomeBasedJavaCompilerFactory to the old one

--- platforms/jvm/language-java/src/main/java/org/gradle/api/internal/tasks/compile/JavaHomeBasedJavaCompilerFactory.java
+++ platforms/jvm/language-java/src/main/java/org/gradle/api/internal/tasks/compile/JavaHomeBasedJavaCompilerFactory.java
@@ -21,17 +21,28 @@ import org.gradle.internal.Factory;
 import java.io.File;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class JavaHomeBasedJavaCompilerFactory implements Factory<ContextAwareJavaCompiler>, Serializable {
-    private final JdkTools jdkTools;
+    private final List<File> compilerPluginsClasspath;
+    // We use a static cache here because we want to reuse classloaders in compiler workers as
+    // it has a huge impact on performance. Previously there was a single, JdkTools.current()
+    // instance, but we can have different "compiler plugins" classpath. For this reason we use
+    // a map, but in practice it's likely there's only one instance in this map.
+    private final static transient Map<List<File>, JdkTools> JDK_TOOLS = new ConcurrentHashMap<>();
 
     public JavaHomeBasedJavaCompilerFactory(List<File> compilerPluginsClasspath) {
-        this.jdkTools = new JdkTools(compilerPluginsClasspath);
+        this.compilerPluginsClasspath = compilerPluginsClasspath;
     }
 
     @Override
     public ContextAwareJavaCompiler create() {
+        JdkTools jdkTools = JavaHomeBasedJavaCompilerFactory.JDK_TOOLS.computeIfAbsent(compilerPluginsClasspath, JavaHomeBasedJavaCompilerFactory::createJdkTools);
         return jdkTools.getSystemJavaCompiler();
     }
 
+    private static JdkTools createJdkTools(List<File> compilerPluginsClasspath) {
+        return new JdkTools(compilerPluginsClasspath);
+    }
 }
